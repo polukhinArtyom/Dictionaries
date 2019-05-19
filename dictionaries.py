e5f95da988202e902e9f5d45ffbe04aa9017e8bb
Artyom Polukhin <50484298+polukhinArtyom@users.noreply.github.com>
2019-05-19 13:04:27 +0300

    Update dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -1,2 +1,13 @@
+print("***********************Использование словаря****************")
 powers = {"The Pigeon": "flight", "Brainzar": "mind reader", "Cyborg": "controls machines"}
 print(powers["The Pigeon"])
+print("*********Добавление элемента в словарь******************")
+powers["Laser Girl"] = "shoot laser"
+print(powers)
+print("*********Удаление элемента в словарь******************")
+del powers["The Pigeon"]
+print(powers)
+print("*********Изменение элемента в словарь******************")
+powers["Brainzar"] = "seening the future"
+print(powers)
+
