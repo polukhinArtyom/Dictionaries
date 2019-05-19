20967924923b6a0dd944d08fb563ce8cb82225bb
Artyom Polukhin <50484298+polukhinArtyom@users.noreply.github.com>
2019-05-19 13:30:36 +0300

    Update alien translator.py

--- alien translator.py
+++ alien translator.py
@@ -1,2 +1,14 @@
-powers = {"The Pigeon": "flight", "Brainzar": "mind reader", "Cyborg": "controls machines"}
-print(powers["The Pigeon"])
+alienDictionary = {"we":"vorag","come":"thang","in":"zon","peace":"argh",
+                   "hello":"kodar","can":"znak","i":"az","borrow":"liftit",
+                   "some":"zum","rocket":"upgoman","fuel":"kakboom","please":"selpin",
+                   "don't":"baaarn","shoot":"flabil","welcome":"unkip","our":"manding",
+                   "new":"brang","alien":"marangin","overlords":"bap"}
+englishPhrase = input("Please enter an English word or phrase to translate: \n")
+englishWords = englishPhrase.lower().split()
+alienWords = []
+for word in englishWords:
+    if word in alienDictionary:
+        alienWords.append(alienDictionary[word])
+    else:
+        alienWords.append(word)
+print("In alien, say:", " ".join(alienWords))
